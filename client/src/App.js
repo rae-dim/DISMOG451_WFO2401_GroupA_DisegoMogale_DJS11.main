9782234c050f2a11ad6e89277913c9addc155e64
Disego <disegorachelm@gmail.com>
2024-06-19 02:16:18 +0200

    add comments for theme toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import {useState} from "react"
-import styled, { ThemeProvider } from "styled-components"
+import {useState} from "react" //allowa us to add state to components for state management.
+import styled, { ThemeProvider } from "styled-components" //styled-components for creating styled components with dynamic theming. 'styled-components is a library that alows us to write CSS-in-JS that looks like a real CSS file, but with all the power of JavaScript
 import "./index.css"
 import { lightTheme, darkTheme } from "./utils/Themes"
 
 const Container = styled.div`
     background: ${({ theme}) => theme.bg};
     width: 100%;
-    height: 100vh;`
+    height: 100vh;
+    `
 
 export default function App() {
-  const {darkMode, setDarkMode} = useState(true) 
+  const {darkMode, setDarkMode} = useState(true) //useState hook used to manage darkMode state. Initializes darkMode as true and provides 'setDarkMode' function to change the state.
 
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkMode? darkTheme : lightTheme}> {/**ThemeProvider is used to pass down the current theme to the styled components */}
     <Container>
       Podtastic
     </Container>
