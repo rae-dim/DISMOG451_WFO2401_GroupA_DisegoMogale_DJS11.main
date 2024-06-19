3d9b2776fa3688f0dfbef28cc3f4145ef05356ab
Disego <disegorachelm@gmail.com>
2024-06-19 04:53:35 +0200

    add themes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,30 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}",
       "./public/index.html",
     ],
+    darkMode: 'class', // Enable to change theme based on system preference
     theme: {
         extend: {
           colors: {
-            dark: '#1a202c', // Replace with your dark theme background color
-            light: '#f7fafc', // Replace with your light theme background color
+            dark: {
+              bg: "#15171E",
+              bgLight: "#1C1E27",
+              primary: "#be1adb",
+              text_primary: "#F2F3F4",
+              text_secondary: "#b1b2b3",
+              card: "#121212",
+              button: "#5c5b5b",
+            },
+            light: {
+              bg: "#FFFFFF",
+              bgLight: "#f0f0f0",
+              primary: "#be1adb",
+              text_primary: "#111111",
+              text_secondary: "#48494a",
+              card: "#FFFFFF",
+              button: "#5c5b5b",
+            },
           },
         },
       },
-    plugins: [],
-  };
+      plugins: [],
+    };
