5acc29302a98122f59a702b69c24d31b11cfdf91
Disego <disegorachelm@gmail.com>
2024-06-19 04:52:59 +0200

    add dark and light theme to tailwind.config

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,17 +2,31 @@ import React from "react"
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 import {HomeRounded, CloseRounded} from "@mui/icons-material"
-
+/* import "./index.css" */
 
 const Menu = styled.div`
     display: flex;
     flex-direction: column;
     height: 100vh;
     background-color: ${({theme}) => theme.bg};
+    color: ${({theme}) => theme.text_primary};
+   /* @media (max-width: 1100px) {
     position: fixed;
+    z-index: 100;
+    width: 100%;
+    max-width: 250px;
+    left: ${({ setMenuOpen}) => (setMenuOpen? '0' : '-250px')};
+    transition: left 0.3s ease-in-out; */
 `
 const Logo = styled.div`
-
+    color: ${({theme}) => theme.primary};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 6px;
+    font-weight: bold;
+    font-size: 24px;
+    margin: 16px 0px;
 `
 const CloseIcon = styled.div`
 
@@ -24,16 +38,31 @@ const Elements = styled.div`
 const Navigation = styled.div`
 
 `
+
+const Flex = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    `
+const Flex2 = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 export default function Sidebar() {
     return (
         <Menu>
-            <Logo>Podtastic</Logo>
-            <CloseIcon>
-                <CloseRounded />
-            </CloseIcon>
+            <Flex>
+                <Logo>PODTASTIC</Logo>
+                <CloseIcon>
+                    <CloseRounded />
+                </CloseIcon>
+            </Flex>
             <Elements>
-                <HomeRounded />
-                <Navigation>Listen Now</Navigation>
+                <Flex2>
+                    <HomeRounded />
+                    <Navigation>Listen Now</Navigation>
+                </Flex2>
             </Elements>
         </Menu>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}",
       "./public/index.html",
     ],
-    theme: {},
+    theme: {
+        extend: {
+          colors: {
+            dark: '#1a202c', // Replace with your dark theme background color
+            light: '#f7fafc', // Replace with your light theme background color
+          },
+        },
+      },
     plugins: [],
   };
