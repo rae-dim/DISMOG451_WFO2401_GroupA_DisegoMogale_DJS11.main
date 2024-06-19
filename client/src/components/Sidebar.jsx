28948719b867a9ae11c72fc78237bf3c4120cd2e
Disego <disegorachelm@gmail.com>
2024-06-19 10:11:56 +0200

    add menuItems and button for mapping

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,29 +1,66 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { HomeRounded, CloseRounded } from "@mui/icons-material";
+import { Link, BrowserRouter } from "react-router-dom";
+import { HomeRounded, CloseRounded, SearchRounded, FavoriteRounded, LightModeRounded, LogoutRounded} from "@mui/icons-material";
 import Logopng from "../images/favicon-32x32.png";
 import "../index.css"; 
+
+const menuItems = [
+  {
+    link: "/",
+    name: "Listen Now",
+    icon: <HomeRounded />
+  },
+
+  {
+    link: "/search",
+    name: "Explore",
+    icon: <SearchRounded />
+  },
+
+  {
+    link: "/favorites",
+    name: "Favorites",
+    icon: <FavoriteRounded />
+  }
+]
+
+const buttons = [
+  {
+    fun: console.log('Toggle theme'),
+    name: "Toggle Theme",
+    icon: <LightModeRounded />
+  },
+
+  {
+    fun: console.log('Log out'),
+    name: "Log Out",
+    icon: <LogoutRounded />
+  }
+]
 
 export default function Sidebar() {
   const [darkMode, setDarkMode] = useState(true); // Example state for toggling theme
 
   return (
+    <BrowserRouter>
     <div className={`flex flex-col h-full w-[25%] gap-2 ${darkMode ? 'bg-[#15171E] text-[#F2F3F4]' : 'bg-[#FFFFFF] text-[#111111]'}`}>
       <div className="flex flex-row items-center justify-between">
         <div className="flex flex-row w-full items-center justify-center gap-6 font-bold text-xl my-4">
           <img className="w-8 h-8 object-contain" src={Logopng} alt="Logo" />
           PODTASTIC
         </div>
-        <div className="hidden md:block">
+        <div className="block md:hidden">
           <CloseRounded />
       </div>
       </div>
-      <div className={`flex flex-row px-4 items-center gap-6 cursor-pointer ${darkMode ? 'text-[#F2F3F4] hover:bg-[#F2F3F4]' : 'text-[#111111] hover:bg-[#E5E7EB]'}`}>
+      
         <Link to="/">
+          <div class Name={`flex flex-row px-4 items-center gap-6 cursor-pointer ${darkMode ? 'text-[#F2F3F4] hover:bg-[#660666]' : 'text-[#111111] hover:bg-[#E5E7EB]'}`}>
           <HomeRounded />
           <div className="py-3">Listen Now</div>
+          </div>
         </Link>
-      </div>
+      
       {/* <button
         className="m-4 p-2 bg-gray-500 text-white rounded"
         onClick={() => setDarkMode(prevMode => !prevMode)}
@@ -31,5 +68,6 @@ export default function Sidebar() {
         Toggle Theme
       </button>*/}
     </div> 
+    </BrowserRouter>
   );
 }
