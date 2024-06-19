fd9b7644f23a6be46edf4c09883224869461602a
Disego <disegorachelm@gmail.com>
2024-06-19 02:00:46 +0200

    implement light and dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import {useState} from "react"
+import styled, { ThemeProvider } from "styled-components"
+import "./index.css"
+import { lightTheme, darkTheme } from "./utils/Themes"
 
+const Container = styled.div`
+    background: ${({ theme}) => theme.bg};
+    width: 100%;
+    height: 100vh;`
 
-function App() {
+export default function App() {
+  const {darkMode, setDarkMode} = useState(true) 
+
   return (
-    <div >
+    <ThemeProvider theme={lightTheme}>
+    <Container>
       Podtastic
-    </div>
+    </Container>
+    </ThemeProvider>
   );
 }
 
-export default App;
