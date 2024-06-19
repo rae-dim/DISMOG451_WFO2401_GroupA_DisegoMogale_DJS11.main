4838476f57ecadde4d53250d7492a44239a462f5
Disego <disegorachelm@gmail.com>
2024-06-19 22:30:45 +0200

    add menu and login icons

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from "react"
 import { useTheme } from "styled-components"
+import { MenuRounded, LoginRounded } from "@mui/icons-material"
 
 export default function NavBar() {
     const theme = useTheme()
+
     return (
-        <div className={`flex justify-between w-full py-4 px-10 items-center gap-7`}
+        <div className="flex justify-between w-full py-4 px-10 items-center gap-8"
 			style={{ color: theme.text_primary, backgroundColor: theme.bg }}>
-			NavBar
+			<div key="menu-icon" className="">
+                <MenuRounded />
+            </div>
+            <div key="login-button" className="flex items-center gap-2 hover:">
+                <LoginRounded />
+                <span className="text-sm">Login</span>
+            </div>
 		</div>
     )
 }
